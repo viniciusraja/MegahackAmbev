c54326af8e0bf469f18f8937af288e375e40c8c9
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-07-01 00:38:45 -0300

    ADD:  Config

---
+++ src/config/constants/Colors.js
@@ -0,0 +1,12 @@
+export default {
+  backgroundColor:"#ffffff",
+  textsPrimary:"#000",
+  textsSecondary:"#999",
+  yellow:"#FFCC00",
+  primaryText:"#FFF",
+  lightGrey:"#b7b7b7",
+  darkShade:"#000000",
+  mediumShade:"#111111",
+  lightShade:"#242424",
+
+};
---
+++ src/config/constants/Constants.js
@@ -0,0 +1,11 @@
+import Colors from './Colors'
+import Layout from './Layout'
+
+const Constants={
+    Colors,
+    fontFamily:'Nunito-Regular',
+    fontFamilyBold:'Nunito-Bold',
+    fontFamilyXBold:'Nunito-Black',
+    Layout
+} 
+export default Constants;
