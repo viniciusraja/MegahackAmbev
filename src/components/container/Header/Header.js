4b75d5dda8fa1fd5a41ffb88e6d8dff64f853032
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-07-01 00:40:18 -0300

    ADD: Header

---
+++ src/components/container/Header/Header.js
@@ -0,0 +1,171 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Animated,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  Easing,
+  Keyboard,
+  Image
+
+} from 'react-native';
+import {
+  MaterialCommunityIcons,
+  FontAwesome,
+  AntDesign,
+} from '@expo/vector-icons';
+import { styles } from './styles';
+import Constants from '../../../config/constants/Constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { showLoginComponent } from 'store/ducks/actions/showComponent';
+import { useNavigation } from 'react-navigation-hooks';
+import Autocomplete from 'react-native-autocomplete-input';
+
+const springValue = new Animated.Value(0);
+const animatedEvent = Animated.event(
+  [
+    {
+      nativeEvent: {
+        scaleSpring: springValue,
+      },
+    },
+  ],
+  { useNativeDriver: true }
+);
+
+const springAnimateBurguer = () => {
+  springValue.setValue(0.9);
+  Animated.spring(springValue, {
+    toValue: 1.3,
+    friction: 2,
+  }).start();
+};
+
+const searchProducts=(search, allProducts)=> {
+  if (search === '') {
+    return [];
+  }
+  search = search.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  const regex = new RegExp(`${search.trim()}`, 'i');
+
+  return allProducts.filter((product) => {
+    return (
+      product.name
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .search(regex) >= 0
+    );
+  });
+}
+
+
+const getTotalPriceOfCart = (cartList) => {
+  if (!!cartList.length) {
+    springAnimateBurguer()
+    const total = cartList
+      .map((item) => item.count)
+      .reduce((price, total) => price + total);
+    return total;
+  }
+  return 0;
+};
+const Header = (props) => {
+  const productsList = useSelector((state) => state.getProductsList.productsList);
+  const userInfo= useSelector(state => (state.getUserInfo.userInfo))
+  const allProducts=[...productsList]
+  const cartList = useSelector((state) => state.getCartList.cartList);
+  const [search, setSearch] = useState('');
+  const dispatch = useDispatch();
+  const { navigate } = useNavigation();
+  const products = searchProducts(search,allProducts);
+  const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
+  let total = getTotalPriceOfCart(cartList);
+  return (
+    <View style={styles.headerContainer}>
+      <TouchableOpacity
+        onPress={() => navigate('CartScreen')}
+        style={styles.cartButtonContainer}>
+        <MaterialCommunityIcons
+          name="cart-outline"
+          size={24}
+          color={Constants.Colors.lightGrey}
+        />
+        {(total>0)&&<Animated.View
+          style={[
+            styles.quantityOfItemsInCartContainer,
+            {
+              transform: [{ scale: springValue }],
+            },
+          ]}>
+          <Text style={styles.quantityOfItemsInCartText}>{total}</Text>
+        </Animated.View>}
+      </TouchableOpacity>
+      <View style={styles.autoCompleteContainerToFixPosition}>
+              <Autocomplete
+                onPress={() => this.myTextInput.current.clear()}
+                autoCapitalize="none"
+                autoCorrect={false}
+                clearButtonMode="always"
+                style={styles.inputSearchProductContainer}
+                inputContainerStyle={styles.inputContainerStyle}
+                containerStyle={styles.containerStyle}
+                listContainerStyle={styles.listContainerStyle}
+                listStyle={styles.listStyle}
+                data={
+                  products.length === 1 && comp(search, products.productsList.name)
+                    ? []
+                    : products
+                }
+                defaultValue={search}
+                onChangeText={(text) => setSearch(text) }
+                keyExtractor={(item) => `${item.id}`}
+                placeholder="Pesquise Seu Produto"
+                renderItem={({ item, index }) => {
+                  if (index < 4) {
+                    return (
+                      <TouchableOpacity
+                        onPress={() => {
+                          setSearch(item.name)
+                            Keyboard.dismiss();
+                        }}>
+                        <View style={styles.inputSearchCountryItemContainer}>
+                          {/* <Image
+                            style={styles.inputSearchCountryFlagImage}
+                            source={{
+                              uri: item.countryInfo.flag,
+                            }}
+                          /> */}
+                          <Text style={styles.inputSearchCountryText}>
+                            {item.name}
+                          </Text>
+                        </View>
+                      </TouchableOpacity>
+                    );
+                  }
+                  return null;
+                }}
+              />
+            </View>
+
+
+
+      <TouchableOpacity
+        onPress={() => dispatch(showLoginComponent())}
+        style={styles.loginButtonContainer}>
+        {!!userInfo.picture?
+          <Image
+          style={{height:30,width:30,borderRadius:10}}
+          source={{uri:userInfo.picture.data.url}}/>
+          :<FontAwesome
+          name="user-circle"
+          size={24}
+          color={Constants.Colors.lightGrey}
+        />
+        }
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Header;
---
+++ src/components/container/Header/styles.js
@@ -0,0 +1,124 @@
+import { StyleSheet } from 'react-native';
+import Constants from '../../../config/constants/Constants';
+
+export const styles = StyleSheet.create({
+  headerContainer: {
+    height:Constants.Layout.headerHeight,
+    width:'100%',
+    flexDirection:'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor:Constants.Colors.backgroundColor
+  },
+  cartButtonContainer:{
+    height:'100%',
+    width:45,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  input:{
+    height:40,
+    width: '70%',
+    paddingLeft:10,
+    textAlign: 'left',
+    color: '#FFF',
+    backgroundColor:'#fff',
+    borderRadius:30,
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 5,
+    opacity:0.7,
+    },
+    quantityOfItemsInCartContainer:{
+    position:'absolute',
+    height:15,
+    width:15,
+    bottom:6,
+    right:0,
+    backgroundColor:Constants.Colors.yellow,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:10,
+    },
+    quantityOfItemsInCartText:{
+      textAlign:'center',
+      color:'#fff',
+      fontFamily:Constants.fontFamily,
+      fontSize:12,
+    },
+    loginButtonContainer:{
+    height:'100%',
+    width:45,
+    justifyContent:'center',
+    alignItems:'center'
+    },
+    
+
+
+
+
+
+
+
+
+    autoCompleteContainerToFixPosition: {
+      position:'absolute',
+      zIndex: 60,
+      width:282,
+      left:(Constants.Layout.window.width-282)/2,
+      top:(Constants.Layout.headerHeight-40)/2,
+      height:Constants.Layout.headerHeight,
+    },
+    inputSearchProductContainer: {
+      height: 40,
+      width: '100%',
+      borderRadius: 20,
+      backgroundColor: '#F5F5F5',
+      elevation: 7,
+      fontFamily: Constants.fontFamily,
+      fontSize: 20,
+      textAlign: 'center',
+    },
+    inputContainerStyle: {
+      height: 29,
+      width: 280,
+      borderWidth: 0,
+      borderRadius: 20,
+    },
+    containerStyle: {
+      
+    },
+    listContainerStyle: {
+      justifyContent:'center',
+      alignItems:'center',
+    },
+    listStyle: {
+      height:600,
+      backgroundColor: 'transparent',
+      borderWidth: 0,
+      marginTop: 2,
+      width: '100%',
+    },
+    inputSearchCountryItemContainer: {
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      height: 60,
+      backgroundColor: '#F5F5F5',
+      borderRadius: 20,
+      marginVertical: 2,
+    },
+    inputSearchCountryFlagImage: {
+      height: 25,
+      width: 35,
+      borderRadius: 7,
+      marginRight: 20,
+      marginLeft: 10,
+    },
+    inputSearchCountryText: {
+      fontFamily:Constants.fontFamily,
+      fontSize: 20,
+      textAlign: 'center',
+    },
+
+});
