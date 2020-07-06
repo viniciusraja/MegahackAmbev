22fa79becc1d6140f369f6fc1559878f905914f9
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-07-05 22:29:10 -0300

    Add: web-build

---
+++ web-build/precache-manifest.d3bd358efdcb940eac34a9180fd6fdad.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "091e225dbd142b05951e",
+    "url": "/static/js/app.d42beb45.chunk.js"
+  },
+  {
+    "revision": "d1394312a5556029b980",
+    "url": "/static/js/runtime~app.65b60541.js"
+  },
+  {
+    "revision": "c5b94939a52e3b1ee9a5",
+    "url": "/static/js/2.2f812f6d.chunk.js"
+  },
+  {
+    "revision": "35d101bd03abdbf36987830e6a872a6b",
+    "url": "/static/media/CoinIcone.35d101bd.svg"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "fd635f13d0f4c7e77dca4081bb42cf1d",
+    "url": "/static/media/CoinIconActive.fd635f13.svg"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/./fonts/Nunito-Regular.ttf"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/./fonts/Nunito-Bold.ttf"
+  },
+  {
+    "revision": "6abf597a1f3f44fd10bdcb7b86b494f0",
+    "url": "/./fonts/Nunito-Black.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "92e0c2e58afb6b6a3aa64c710f35113c",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "b0223f7ef7ec6ed56ec447c2f0209e39",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "6ae1e33dff97d7dd4f42c2d2f17f0e13",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5576ceb7e8ba42e4d1d80b7dbdb06705",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4ff7093a37f111c2900aa04b0c34a79",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
